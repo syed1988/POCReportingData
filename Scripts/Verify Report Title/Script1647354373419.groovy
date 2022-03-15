dead1d809ad135001a839dea5439b38ad4145a4c
Syed <syhussain@teksystems.com>
2022-03-15 12:13:51 -0500

    updated url

--- Scripts/Verify Report Title/Script1647354373419.groovy
+++ Scripts/Verify Report Title/Script1647354373419.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('https://app.powerbi.com/singleSignOn?route=groups%2f7b4737be-1aed-4acc-9954-40569989ca94%2freports%2f8917e9f0-7af8-49d1-9053-5feaf089889d%2fReportSection&ru=https:%2f%2fapp.powerbi.com%2f%3froute%3dgroups%252f7b4737be-1aed-4acc-9954-40569989ca94%252freports%252f8917e9f0-7af8-49d1-9053-5feaf089889d%252fReportSection%26noSignUpCheck%3d1')
+WebUI.openBrowser('https://app.powerbi.com/groups/7b4737be-1aed-4acc-9954-40569989ca94/reports/8917e9f0-7af8-49d1-9053-5feaf089889d/ReportSection')
 
 WebUI.waitForElementVisible(findTestObject('Email'), 20)
 
@@ -25,7 +25,7 @@ WebUI.setText(findTestObject('Email'), GlobalVariable.email)
 
 WebUI.click(findTestObject('button_Submit'))
 
-WebUI.setText(findTestObject('password'), GlobalVariable.pw)
+WebUI.setText(findTestObject('password'), 'Tek&asp2022')
 
 WebUI.click(findTestObject('sign-in button'))
 
@@ -43,7 +43,7 @@ WebUI.setText(findTestObject('Email'), GlobalVariable.email)
 
 WebUI.click(findTestObject('button_Submit'))
 
-WebUI.setText(findTestObject('password'), GlobalVariable.pw)
+WebUI.setText(findTestObject('password'), 'Tek&asp2022')
 
 WebUI.click(findTestObject('sign-in button'))
 
